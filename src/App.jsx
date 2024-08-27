cc5227202b52667b9aa4f06036e0e352c99f0055
Manmohan Hansda <hmanmohan12@gmail.com>
2024-08-27 09:44:09 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState("");
+  const [amount, setAmount] = useState("");
+  const [expenses, setExpenses] = useState([]);
+
+  const addExpense = () => {
+    if (!input || !amount) return;
+
+    const newExpense = {
+      id: expenses.length + 1,
+      title: input,
+      amount: amount,
+    };
+    setExpenses([...expenses, newExpense]);
+    setInput("");
+    setAmount("");
+  };
+
+  const deleteExpense = (id) => {
+    setExpenses(expenses.filter((expense) => expense.id !== id));
+  };
 
   return (
-    <div>
-      Hello there!
+    <div className="container">
+      <h2>Expense Tracker</h2>
+      <div className="block">
+        <input
+          type="text"
+          value={input}
+          placeholder="Expense"
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <input
+          type="text"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          placeholder="Amount"
+        />
+        <button onClick={addExpense}>Add Expense</button>
+      </div>
+      <ul className="expense-list">
+        {expenses.map((expense) => (
+          <li key={expense.id}>
+            <span>{expense.title}</span>
+            <span>{expense.amount}</span>
+            <button onClick={() => deleteExpense(expense.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
